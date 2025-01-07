21a62fde5656d99bb30fae4ce32578bc958d115b
Thanh21062006 <caotatthanh2006@gmail.com>
2025-01-07 17:01:38 +0700

    Update BT3.cpp

--- BT3.cpp
+++ BT3.cpp
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
-int total(int *a, int *b);
+void total(int a, int b, int *sum);
 
 int main() {
-	int a = 10, b = 15, sum;
-	sum = total(&a, &b);
+	int a = 10, b = 15, sum = 0;
+	total(a, b, &sum);
 	printf("Tong %d va %d la: %d", a, b, sum);
 	
 	return 0;
 }
 
-int total(int *a, int *b){
-	int total = *a+*b;
-	
-	return total;
-};
+void total(int a, int b, int *sum) {
+	*sum = a + b;
+}
