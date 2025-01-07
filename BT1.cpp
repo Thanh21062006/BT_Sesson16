c88f6b826852db41ed8ab8e983519a31e61ce427
Thanh21062006 <caotatthanh2006@gmail.com>
2025-01-07 16:21:52 +0700

    Update BT1.cpp

--- BT1.cpp
+++ BT1.cpp
@@ -6,8 +6,10 @@ int main() {
 	
 	//Cach 1:
 	printf("Gia tri x la: %d\n", x);
+	printf("Dia chi x la: %lu\n", &x);
 	//Cach 2:
 	printf("Gia tri x la: %d\n", *xprt);
+	printf("Dia chi xprt la: %lu\n", xprt);
 	
 	return 0; 
 }
